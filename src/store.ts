3ce0fcb0cd76f4b949cb81e970501cfa41878ce4
Kwangwook Koh <kwkoh_cool@hotmail.com>
2020-10-03 01:23:43 +0900

    add redux-logger package

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,13 @@
 import { rootReducer } from './rootReducer';
-import { devToolsEnhancer } from 'redux-devtools-extension';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { logger } from 'redux-logger';
+//import { devToolsEnhancer } from 'redux-devtools-extension';
+//import { createStore } from 'redux';
 
-export const store = createStore(rootReducer, devToolsEnhancer());
+// export const store = createStore(rootReducer, devToolsEnhancer({}));
+
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(logger))
+);
