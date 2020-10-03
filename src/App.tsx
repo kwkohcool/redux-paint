8093ea31385466fd06c4a210be6b3129d48a81d9
Kwangwook Koh <kwkoh_cool@hotmail.com>
2020-10-03 15:54:39 +0900

    add drawing feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,70 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { currentStrokeSelector } from './selectors';
+import { beginStroke, updateStroke, endStroke } from './actions';
+import { drawStroke } from './canvasUtils';
+
+const WIDTH = 1024;
+const HEIGHT = 768;
 
 function App() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const currentStroke = useSelector(currentStrokeSelector);
+    const dispatch = useDispatch();
+    const isDrawing = !!currentStroke.points.length;
 
-    return <canvas ref={canvasRef} />;
+    const getCanvasWithContext = (canvas = canvasRef.current) => {
+        return { canvas, context: canvas?.getContext('2d') };
+    };
+
+    useEffect(() => {
+        const { context } = getCanvasWithContext();
+        if (!context) {
+            return;
+        }
+        requestAnimationFrame(() =>
+            drawStroke(context, currentStroke.points, currentStroke.color)
+        );
+    }, [currentStroke]);
+
+    const startDrawing = ({
+        nativeEvent,
+    }: React.MouseEvent<HTMLCanvasElement>) => {
+        const { offsetX, offsetY } = nativeEvent;
+        dispatch(beginStroke(offsetX, offsetY));
+    };
+
+    const endDrawing = () => {
+        if (isDrawing) {
+            dispatch(endStroke());
+        }
+    };
+
+    const draw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
+        if (!isDrawing) {
+            return;
+        }
+        const { offsetX, offsetY } = nativeEvent;
+        dispatch(updateStroke(offsetX, offsetY));
+    };
+
+    return (
+        <div className="window">
+            <div className="title-bar">
+                <div className="title-bar-text">Redux Paint</div>
+                <div className="title-bar-controls">
+                    <button aria-label="Close" />
+                </div>
+            </div>
+            <canvas
+                onMouseDown={startDrawing}
+                onMouseUp={endDrawing}
+                onMouseOut={endDrawing}
+                onMouseMove={draw}
+                ref={canvasRef}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,9 +1,46 @@
-type RootState = {};
+import { RootState } from './types';
+import { Action, BEGIN_STROKE, UPDATE_STROKE, END_STROKE } from './actions';
 
-type Action = {
-    type: string;
+const initialState: RootState = {
+    currentStroke: { points: [], color: '#000' },
+    strokes: [],
+    historyIndex: 0,
 };
 
-export const rootReducer = (state: RootState = {}, action: Action) => {
-    return state;
+export const rootReducer = (
+    state: RootState = initialState,
+    action: Action
+) => {
+    switch (action.type) {
+        case BEGIN_STROKE: {
+            return {
+                ...state,
+                currentStroke: {
+                    ...state.currentStroke,
+                    points: [action.payload],
+                },
+            };
+        }
+        case UPDATE_STROKE: {
+            return {
+                ...state,
+                currentStroke: {
+                    ...state.currentStroke,
+                    points: [...state.currentStroke.points, action.payload],
+                },
+            };
+        }
+        case END_STROKE: {
+            if (!state.currentStroke.points.length) {
+                return state;
+            }
+            return {
+                ...state,
+                currentStroke: { ...state.currentStroke, points: [] },
+                strokes: [...state.strokes, state.currentStroke],
+            };
+        }
+        default:
+            return state;
+    }
 };
